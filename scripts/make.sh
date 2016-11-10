695b2943e94486b8ffdeb3daf7d1852220369f31
Vamshi Basupalli <vamshi@cs.wisc.edu>
2016-11-09 18:52:18 -0600

    Fixed bug in scripts/main.sh

--- scripts/make.sh
+++ scripts/make.sh
@@ -11,17 +11,20 @@ function main {
     (
         cd "$release_dir"
     
-        if [[ ! -d "requests" ]]; then
-
             wget https://pypi.python.org/packages/ea/03/92d3278bf8287c5caa07dbd9ea139027d5a3592b0f4d14abf072f890fab2/requests-2.11.1-py2.py3-none-any.whl#md5=b4269c6fb64b9361288620ba028fd385 && \
                 unzip requests-2.11.1-py2.py3-none-any.whl
-                
+        if [[ ! -d "requests" ]]; then
+            echo "$failed to get 'requests' library" && exit 1;
         fi
 
-        if [[ ! -d "$pdir/swamp_api" ]]; then
-            wget https://github.com/vamshikr/swamp-python-api/archive/master.zip \
-                 -O swamp-python-api-master.zip && \
-                cp -r swamp-python-api-master/src/swamp_api .
+
+        wget https://github.com/vamshikr/swamp-python-api/archive/master.zip \
+             -O swamp-python-api-master.zip && \
+            unzip swamp-python-api-master.zip && \
+            cp -r swamp-python-api-master/src/swamp_api .
+        
+        if [[ ! -d "swamp_api" ]]; then
+            echo "$failed to get 'swamp_api (https://github.com/vamshikr/swamp-python-api)' library" && exit 1;
         fi
 
         cp ../src/lambda_function.py ../src/github.py ../resources/user-info.conf .
